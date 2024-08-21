953fca51b9de5c64c146ef7b9c8c0cb67fb6bf39
annutix <annutixx@gmail.com>
2024-08-21 16:38:56 +0700

    add brain-even

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 [![Actions Status](https://github.com/annutix/python-project-49/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/annutix/python-project-49/actions)
 
 <a href="https://codeclimate.com/github/annutix/python-project-49/maintainability"><img src="https://api.codeclimate.com/v1/badges/8344561a4e1e452e947c/maintainability" /></a>
+
+[![asciicast](https://asciinema.org/a/Rpa0IyVJB31LFVI1Vwn024HZb.svg)](https://asciinema.org/a/Rpa0IyVJB31LFVI1Vwn024HZb)
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,39 @@
+from random import randint
+from brain_games.cli import welcome_user
+import prompt
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    brain_even()
+
+
+if __name__ == '__main__':
+    main()
+
+
+def brain_even(questions_to_ask=3):
+    name = welcome_user()
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    for question_num in range(questions_to_ask):
+        user_was_correct: bool = ask_user_question(name)
+        if not user_was_correct:
+            break
+    if question_num == questions_to_ask - 1:
+        print(f'Congratulations, {name}!')
+
+
+def ask_user_question(name):
+    number = randint(0, 100)
+    correct_answer = 'yes' if number % 2 == 0 else 'no'
+    print('Question: ' + str(number))
+    user_answer = prompt.string('Your answer: ')
+    result = user_answer == correct_answer
+    if result:
+        print('Correct!')
+    else:
+        print(
+            f"'{user_answer}' is wrong answer ;(. "
+            + f"Correct answer was '{correct_answer}'.\n"
+            + f"Let's try again, {name}!")
+    return result
